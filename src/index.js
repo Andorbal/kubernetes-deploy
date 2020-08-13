aa075f5f709cf303631b8533fe1908e5fbfe21f8
Andrew Benz <andorbal@gmail.com>
2020-08-13 11:57:06 -0500

    Add redis service

--- src/index.js
+++ src/index.js
@@ -1,9 +1,30 @@
+const { promisify } = require("util");
 const express = require('express')
+const redis = require("redis");
 const app = express()
-const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const port = process.env.WEB_PORT;
+const redisHost = process.env.REDIS_HOST;
+
+const client = redis.createClient({ host: redisHost });
+const getAsync = promisify(client.get).bind(client);
+const setAsync = promisify(client.set).bind(client);
+
+client.on("error", function (error) {
+  console.error(error);
+});
+
+app.get('/', async (req, res) => {
+  const value = await getAsync("hit");
+
+  res.send(`Hello World!  ${value} hits`);
+})
+
+app.get('/hit', async (req, res) => {
+  const value = parseInt(await getAsync("hit"), 10) || 0;
+  await setAsync("hit", value + 1);
+
+  res.send(`Hit recorded!`);
 })
 
 app.listen(port, () => {
